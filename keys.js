bb4457fe31d4f621e446854ad17940d6ecd52df3
Annyv <annybella18@gmail.com>
2017-01-31 20:18:49 -0500

    progress made with spotify, movie this and twitter

--- keys.js
+++ keys.js
@@ -1,8 +1,10 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<	q32JdY43FvRK3rpa2iYycB7Gt>',
-  consumer_secret: '<mNeVo02h2KHtiFaF8IcbYv5XtKiXqW3JogMq5VQYbZVi7OAFre>',
-  access_token_key: '<	824263221439827974-yKBG08UB8zTXXAszwJ8DITPKRzUQVRD>',
-  access_token_secret: '< VQBj45oHraxIGWn7ZaLv415C2EyAsioxk9uYU1ukYVdtj>',
+  consumer_key: 'q32JdY43FvRK3rpa2iYycB7Gt',
+  consumer_secret: 'mNeVo02h2KHtiFaF8IcbYv5XtKiXqW3JogMq5VQYbZVi7OAFre',
+  access_token_key: '824263221439827974-yKBG08UB8zTXXAszwJ8DITPKRzUQVRD',
+  access_token_secret: 'VQBj45oHraxIGWn7ZaLv415C2EyAsioxk9uYU1ukYVdtj'
 }
+
+
--- liri.js
+++ liri.js
@@ -1,16 +1,86 @@
-// Grabs the bands variables
 var keys = require("./keys.js");
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var omd = require('request');
+var movieName ="";
+var command = process.argv[2];
+var queryUrl = "http://www.omdbapi.com/?t="+ movieName +"&y=&plot=short&r=json";
+var nodeArgs = process.argv;
 
-// Gets all of myBands bands from the bands file.
-var keysList = keys.twitterKeys;
-var latersTweets = "";
+function getMovie(){
+	for (var i = 2; i < nodeArgs.length; i++) {
 
-var myTweets = process.argv[2];
-console.log(myTweets);
-var twitterUrl = "https://api.twitter.com/1.1/search/tweets.json?q=" + laters tweets + "&y=&result_type=mixed&count=40&r=json";
+  if (i > 2 && i < nodeArgs.length) {
 
-console.log(twitterUrl, function(error, response, body){
-	if (!error  && response.statuscode === 200) {
+    movieName = movieName + "+" + nodeArgs[i];
 
-	}
+  }
+
+  else {
+
+    movieName += nodeArgs[i];
+
+  }
+}
+
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Release Year: " + JSON.parse(body).Year);
+  }
 });
+
+}
+
+function getSong(){
+
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ console.log(data);
+    // Do something with 'data' 
+});
+}
+
+
+function myTweets(){
+
+var client = new Twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
+});
+ 
+var params = {screen_name: 'annybella18',
+				count: 20
+	};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+}
+
+// ------------------------------Program stars here-------------------------------
+
+if( command == "my-tweets") {
+	 myTweets();
+} 
+	if(command == "spotify-this-song"){
+		getSong();
+	}
+
+if ( command == "movie-this"){
+	getMovie();
+}
